dce34bb46f048fe73bcfb4a402a3eeb80bf9d09b
kevin-proctor <kevin.proctor26@gmail.com>
2020-02-07 15:11:07 -0600

    added facts api html and ts pages

---
+++ facts.ts
@@ -0,0 +1,6 @@
+function login() {
+    let username: HTMLElement = document.getElementById("username");
+    let password: HTMLElement = document.getElementById("password");
+    let usernames: string[];
+    let passwords: string[];
+};
